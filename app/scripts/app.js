3b7a407db74ada7abce1651886bb1a1d7fbefc19
george <ruan3@illinois.edu>
2015-10-17 01:01:18 -0500

    Initial

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,11 +11,10 @@
     .module('EZSched', ['ngRoute'])    // Set up app dependencies
     .config(function($routeProvider) {
       $routeProvider
-        /*.when('/', {
-          templateUrl: 'views/home.html',
-          controller: 'HomeCtrl'
+        .when('/', {
+          templateUrl: 'views/login.html',
+          controller: 'LoginCtrl'
         })
-        */
         .when('/404', {
           templateUrl: '404.html'
         })
---
+++ app/scripts/controllers/LoginCtrl.js
@@ -0,0 +1,22 @@
+/**
+ * Created by George Ruan on October 17, 2015.
+ *
+ * HomeCtrl defines the behavior of the Home Page.
+ */
+
+(function() {
+  'use strict';
+
+  angular.module('EZSched')
+    .controller('LoginCtrl', LoginCtrl);
+
+    //homeCtrl.$inject = ['$scope', '$interval', 'UserData'];
+
+    function LoginCtrl () {
+      document.getElementById('toggleProfile').addEventListener('click', function() {
+        [].map.call(document.querySelectorAll('.profile'), function(el) {
+          el.classList.toggle('profile--open');
+        });
+      })
+    }
+})();
